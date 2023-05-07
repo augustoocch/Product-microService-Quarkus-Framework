111096fef4597ae142e2af37f47fc51441e435bc
augustoocch <augusto.occh@gmail.com>
2023-05-07 15:11:01 -0300

    Added config to date time formater

--- src/main/java/org/augustoocc/config/ConfigProperties.java
+++ src/main/java/org/augustoocc/config/ConfigProperties.java
@@ -1,4 +1,16 @@
 package org.augustoocc.config;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.ws.rs.Produces;
+import java.time.format.DateTimeFormatter;
+
 public class ConfigProperties {
+
+    @Produces
+    DateTimeFormatter dateTimeFormatter () {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+    }
+
+
 }
